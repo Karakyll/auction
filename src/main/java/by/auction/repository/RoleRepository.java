895407279c20d16cbebd5f4896c49d34c54e9c0e
Karakyll <dergizzor@gmail.com>
2018-04-10 17:46:13 +0300

    Add role entity map

---
+++ src/main/java/by/auction/repository/RoleRepository.java
@@ -0,0 +1,10 @@
+package by.auction.repository;
+
+import by.auction.entity.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoleRepository extends JpaRepository<Role, Long>{
+
+
+
+}
