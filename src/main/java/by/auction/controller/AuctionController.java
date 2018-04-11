c55a7fa879a359613f206eaa1dd4b5959ed8a0de
Karakyll <dergizzor@gmail.com>
2018-04-11 19:38:39 +0300

    Fill auction controller with methods

--- src/main/java/by/auction/controller/AuctionController.java
+++ src/main/java/by/auction/controller/AuctionController.java
@@ -1,13 +1,21 @@
 package by.auction.controller;
 
 import by.auction.entity.Auction;
+import by.auction.entity.Category;
+import by.auction.entity.Product;
+import by.auction.entity.User;
 import by.auction.service.AuctionService;
+import by.auction.service.CategoryService;
+import by.auction.service.ProductService;
+import by.auction.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
+import java.util.Date;
 import java.util.List;
 
 @RestController
@@ -18,9 +26,104 @@ public class AuctionController {
     @Autowired
     private AuctionService auctionService;
 
+    @Autowired
+    private CategoryService categoryService;
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private ProductService productService;
+
     @RequestMapping(method = RequestMethod.GET)
-    List<Auction> getAllOngoingAuctions() {
-        return this.auctionService.findFinished(false);
+    ResponseEntity getAllAuctions() {
+        return ResponseEntity.ok(auctionService.findAll());
+    }
+
+    @RequestMapping(value = "/{auctionId:[\\d]+}", method = RequestMethod.GET)
+    ResponseEntity getAuctionById(@PathVariable Long auctionId) {
+        if (auctionService.findById(auctionId).isPresent()) {
+            return ResponseEntity.ok(auctionService.findById(auctionId));
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @RequestMapping(params = "finished", method = RequestMethod.GET)
+    ResponseEntity getAllOngoingAuctions(@RequestParam("finished") boolean finished) {
+        return ResponseEntity.ok(auctionService.findFinished(finished));
+    }
+
+    @RequestMapping(params = "category", method = RequestMethod.GET)
+    ResponseEntity getAuctionsByCategory(@RequestParam("category") String category) {
+        if (categoryService.findByName(category).isPresent()) {
+            return ResponseEntity.ok(auctionService.findByCategoryName(category));
+        } else {
+            return ResponseEntity.badRequest().build();
+        }
+    }
+
+    @RequestMapping(params = "search", method = RequestMethod.GET)
+    ResponseEntity getAuctionsWithProductsContain(@RequestParam("search") String search) {
+        return ResponseEntity.ok(auctionService.findByProductNameContains(search));
+    }
+
+    @RequestMapping(params = "user", method = RequestMethod.GET)
+    ResponseEntity getAuctionsByUserName(@RequestParam("user") String userName) {
+        if (userService.findByUserName(userName).isPresent()) {
+            return ResponseEntity.ok(auctionService.findByUserName(userName));
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @RequestMapping(params = "endBefore", method = RequestMethod.GET)
+    ResponseEntity getAuctionsByEndTime(@RequestParam("endBefore") @DateTimeFormat(pattern="dd.MM.yyyyhh:mm") Date date) {
+        return ResponseEntity.ok(auctionService.findByEndTimeLessThan(date));
+    }
+
+    @RequestMapping(method = RequestMethod.POST)
+    ResponseEntity saveAuction(@RequestBody Auction auction) {
+        if(!userService.findByUserName(auction.getOwner_name()).isPresent()
+                || !categoryService.findByName(auction.getProduct().getCategory_name()).isPresent()) {
+            return ResponseEntity.unprocessableEntity().build();
+        }
+
+        Product newProduct = new Product();
+
+        newProduct.setName(auction.getProduct().getName());
+        newProduct.setCategory(new Category(auction.getProduct().getCategory_name()));
+        newProduct.setPrice(auction.getProduct().getPrice());
+        newProduct.setDescription(auction.getProduct().getDescription());
+
+        newProduct = productService.save(newProduct);
+
+        Auction result = new Auction();
+
+        result.setOwner(userService.findByUserName(auction.getOwner_name()).get());
+        result.setProduct(newProduct);
+        result.setCreateTime(auction.getCreateTime());
+        result.setEndTime(auction.getEndTime());
+        result.setDescription(auction.getDescription());
+        result.setFinished(auction.getFinished());
+
+        result = auctionService.save(result);
+
+        URI location = ServletUriComponentsBuilder
+                .fromCurrentRequest().path("/{auctionId}")
+                .buildAndExpand(result.getId()).toUri();
+
+        return ResponseEntity.created(location).body(result);
+    }
+
+    @RequestMapping(value = "/{auctionId:[\\d]+}", method = RequestMethod.DELETE)
+    ResponseEntity deleteAuction(@PathVariable Long auctionId) {
+        if (auctionService.findById(auctionId).isPresent()) {
+            auctionService.deleteById(auctionId);
+            return ResponseEntity.ok().build();
+        } else {
+            return ResponseEntity.notFound().build();
+        }
     }
 
 }
