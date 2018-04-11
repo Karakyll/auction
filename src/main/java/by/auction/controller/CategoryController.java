5e7c9de3667e69909a8e8ba8f4d7ecf55ec62c64
Karakyll <dergizzor@gmail.com>
2018-04-11 22:15:43 +0300

    fill category controller with methods

--- src/main/java/by/auction/controller/CategoryController.java
+++ src/main/java/by/auction/controller/CategoryController.java
@@ -3,10 +3,8 @@ package by.auction.controller;
 import by.auction.entity.Category;
 import by.auction.service.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -19,8 +17,27 @@ public class CategoryController {
     private CategoryService categoryService;
 
     @RequestMapping(method = RequestMethod.GET)
-    List<Category> getAllCategory() {
-        return categoryService.findAll();
+    ResponseEntity getAllCategory() {
+        return ResponseEntity.ok(categoryService.findAll());
+    }
+
+    @RequestMapping(method = RequestMethod.POST)
+    ResponseEntity saveCategory(@RequestBody Category category) {
+        if (categoryService.findByName(category.getName()).isPresent()) {
+            return ResponseEntity.unprocessableEntity().build();
+        } else {
+            return ResponseEntity.ok(categoryService.save(category));
+        }
+    }
+
+    @RequestMapping(params = "delete", method = RequestMethod.DELETE)
+    ResponseEntity deleteCategory(@RequestParam("delete") String category) {
+        if (categoryService.findByName(category).isPresent()) {
+            categoryService.deleteByName(category);
+            return ResponseEntity.ok().build();
+        } else {
+            return ResponseEntity.notFound().build();
+        }
     }
 
 }
