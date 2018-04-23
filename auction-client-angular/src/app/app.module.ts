962f56ad95833398b0cb4c11a1234148386aa2d5
Karakyll <dergizzor@gmail.com>
2018-04-23 16:25:41 +0300

    initialize category service. initialize category component

--- auction-client-angular/src/app/app.module.ts
+++ auction-client-angular/src/app/app.module.ts
@@ -6,20 +6,24 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { AuctionComponent } from './components/auction/auction/auction.component';
 import { ProductComponent } from "./components/product/product/product.component";
+import { CategoryComponent } from './components/category/category/category.component';
 
 import { AuctionService } from "./services/auction/auction.service";
 import { ProductService } from "./services/product/product.service";
+import { CategoryService } from "./services/category/category.service";
 
 const appRoutes: Routes = [
   {path:'auctions', component:AuctionComponent},
-  {path:'products', component:ProductComponent}
+  {path:'products', component:ProductComponent},
+  {path:'categories', component:CategoryComponent}
 ];
 
 @NgModule({
   declarations: [
     AppComponent,
     AuctionComponent,
-    ProductComponent
+    ProductComponent,
+    CategoryComponent
   ],
   imports: [
     BrowserModule,
@@ -28,7 +32,8 @@ const appRoutes: Routes = [
   ],
   providers: [
     AuctionService,
-    ProductService
+    ProductService,
+    CategoryService
   ],
   bootstrap: [AppComponent]
 })
